2feee6a3e0128289e3c5174e07a327db4e5c3036
Nate <daviesnathanael45@gmail.com>
2026-02-26 21:54:30 +0000

    added buttons so you can go between cards in the same subject that is loaded, also there is an answer button but does nothing as of now

--- public/script.js
+++ public/script.js
@@ -2,14 +2,17 @@ subjects_selector = document.getElementById('subjects_selector');
 subject_text = document.getElementById('subject_text');
 
 card_title = document.getElementById('title');
+card_info = document.getElementById('info');
 
 let cards_list = [];
 let current_subject;
+let current_card = 0;
 
 
 //function for loading the next card
 function loadCard(cardNum){
     card_title.textContent = cards_list[cardNum].title;
+    card_info.textContent = cards_list[cardNum].info;
 }
 
 function loadSubjectList(){
@@ -21,12 +24,18 @@ function loadSubjectList(){
             data.cards.forEach(item => {
                 if (item.subject === current_subject) cards_list.push(item);
             });
-            console.log(cards_list);
 
             //load the first card that has the correct subject
-            card_title.textContent = cards_list[0].title;
+            loadCard(0);
         });
+}
 
+//for chosing next card by pressing the button
+function switchCard(num){
+    current_card += num;
+    if (current_card === -1)current_card = 0;
+    if (current_card === cards_list.length) current_card --;
+    loadCard(current_card);
 }
 
 //fetch inititally 
